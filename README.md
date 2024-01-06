c2293d438d6004cf219916965a95b68bd1bd1dfb
Code Help Tutor <1823890830@qq.com>
2024-01-06 13:00:15 +0000

    WeChat cstutorcs FIT2081 Mobile application development

---
+++ README.md
@@ -0,0 +1,11 @@
+# FIT2081 Mobile application development
+
+# WeChat: cstutorcs
+
+# QQ: 749389476
+
+# Email: tutorcs@163.com
+
+# Computer Science Tutor
+
+# Programming Code Help
---
+++ week5/ScrollableOptionsMenus/app/src/test/java/com/fit2081/scrollableoptionsmenus/ExampleUnitTest.java
@@ -0,0 +1,20 @@
+WeChat: cstutorcs
+QQ: 749389476
+Email: tutorcs@163.com
+package com.fit2081.scrollableoptionsmenus;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() {
+        assertEquals(4, 2 + 2);
+    }
+}
